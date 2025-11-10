babc91722d97d48191adb6fa0f771c3707b51b8b
Juanm0 <juanm.navarro05@gmail.com>
2025-11-10 19:02:17 -0300

    creamos navbar

---
+++ src/components/ItemDetail.jsx
@@ -0,0 +1,11 @@
+
+
+function ItemDetail(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ItemDetail
---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+function Navbar(){
+    return (
+        <nav className='navbar'>
+            <ul>
+                <li>Categorias</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
