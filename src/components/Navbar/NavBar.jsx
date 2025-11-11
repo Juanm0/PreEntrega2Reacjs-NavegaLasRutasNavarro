4417518d14a170afa6848b50b3bd8e557ea7b639
Juanm0 <juanm.navarro05@gmail.com>
2025-11-11 16:32:12 -0300

    cambios en la nav

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,15 +1,21 @@
+import { NavLink } from 'react-router'
 import CartWidget from '../CartWidget/CartWidget'
 import './Navbar.css'
 
-function Navbar(){
+
+function Navbar (){
     return (
-        <nav className='navbar'>
-            <ul className = 'links'>
-                <li className= 'link'>Categorias</li>
-                <li className= 'link'>Contacto</li>
-            </ul>
-            <CartWidget/>
-        </nav>
+                <div className='navbar-container'>
+                    <img className='navbar-icon' src='./src/assets/trash-icon.png'/>
+                    <nav className='navbar'>
+                        
+                        <ul className='links'>
+                            <li className='link'>Categorias</li>
+                            <li className='link'><NavLink to="/contact">Contactos</NavLink></li>
+                        </ul>
+                        <CartWidget/>
+                    </nav>
+                </div>
     )
 }
 
